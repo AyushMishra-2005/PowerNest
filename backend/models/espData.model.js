fd1cb03fe1fefbc0cbe6837f8d90c8a7588fac3d
AyushMishraFeb2005 <ayushmishra9449@gmail.com>
2025-12-24 15:11:55 +0530

    feat: create EspData model with ESP pin mappings

---
+++ backend/models/espData.model.js
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const espDataSchema = new Schema(
+  {
+    blockId:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Block",
+      required: true
+    },
+    allEspPins:{
+      type: [Number],
+      default:[4, 5, 12, 13, 14, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33]
+    },
+    connectedPins:{
+      type:[
+        {
+          sensorEspPin:{
+            type: Number,
+            required: true,
+          },
+          roomEspPin:{
+            type: Number,
+            required: true,
+          }
+        },
+      ],
+      required: true,
+      default:[]
+    }
+  }
+);
+
+const EspData = mongoose.model("EspData", espDataSchema);
+
+export default EspData;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
