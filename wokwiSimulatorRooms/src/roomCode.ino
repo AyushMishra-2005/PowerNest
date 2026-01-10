7f3ff759d177e2666ae2092866b115c5b259b0eb
AyushMishraFeb2005 <ayushmishra9449@gmail.com>
2026-01-10 17:43:27 +0530

    feat: add PIR heartbeat and relay auto-off timeout logic

--- wokwiSimulatorRooms/src/roomCode.ino
+++ wokwiSimulatorRooms/src/roomCode.ino
@@ -10,42 +10,48 @@ const char* mqtt_server = "broker.hivemq.com";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+
 int relayPins[] = {
   4, 5, 12, 13, 14,
   18, 19, 21, 22, 23,
   25, 26, 27,
   32, 33
 };
+
 int pinCount = sizeof(relayPins) / sizeof(relayPins[0]);
+
+unsigned long lastActiveTime[40];
+const unsigned long RELAY_TIMEOUT = 4000; 
 
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg;
-  for (int i = 0; i < length; i++) msg += (char)payload[i];
+  for (int i = 0; i < length; i++) {
+    msg += (char)payload[i];
+  }
 
-  int pin = String(topic).substring(String(topic).lastIndexOf("/") + 1).toInt();
+  int pin = String(topic)
+              .substring(String(topic).lastIndexOf("/") + 1)
+              .toInt();
 
   if (msg == "active") {
     digitalWrite(pin, HIGH);
-  } else {
-    digitalWrite(pin, LOW);
+    lastActiveTime[pin] = millis(); 
+    Serial.println("Relay PIN " + String(pin) + " -> ON");
   }
-
-  Serial.println("Relay PIN " + String(pin) + " -> " + msg);
 }
 
 void connectWiFi() {
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 }
 
 void connectMQTT() {
   while (!client.connected()) {
-    Serial.print("Connecting MQTT...");
     if (client.connect(ESP_ID)) {
-      Serial.println("connected");
       client.subscribe("powernest/ESP_RELAY_ROOM_101/relay/+");
     } else {
-      Serial.println("failed, retrying...");
       delay(2000);
     }
   }
@@ -57,19 +63,33 @@ void setup() {
   for (int i = 0; i < pinCount; i++) {
     pinMode(relayPins[i], OUTPUT);
     digitalWrite(relayPins[i], LOW);
+    lastActiveTime[relayPins[i]] = 0;
   }
 
   connectWiFi();
   client.setServer(mqtt_server, 1883);
-  client.setBufferSize(512);   
   client.setCallback(callback);
   connectMQTT();
 
-  Serial.println("RELAY ESP READY");
+  Serial.println("RELAY ESP READY: " ESP_ID);
 }
 
 void loop() {
   if (!client.connected()) connectMQTT();
   client.loop();
-  delay(10);   
+
+  unsigned long now = millis();
+
+  for (int i = 0; i < pinCount; i++) {
+    int pin = relayPins[i];
+
+    if (digitalRead(pin) == HIGH &&
+        now - lastActiveTime[pin] > RELAY_TIMEOUT) {
+
+      digitalWrite(pin, LOW);
+      Serial.println("Relay PIN " + String(pin) + "auto OFF");
+    }
+  }
+
+  delay(10);
 }
--- wokwiSimulatorSensors/src/sensorCode.ino
+++ wokwiSimulatorSensors/src/sensorCode.ino
@@ -5,14 +5,13 @@
 
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
-
 const char* mqtt_server = "broker.hivemq.com";
 const int mqtt_port = 1883;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// ALL PIR PINS
+
 int pirPins[] = {
   4, 5, 12, 13, 14,
   18, 19, 21, 22, 23,
@@ -21,11 +20,17 @@ int pirPins[] = {
 };
 
 int pirCount = sizeof(pirPins) / sizeof(pirPins[0]);
+
 int lastState[20];
+unsigned long lastSend[20];
+
+const unsigned long HEARTBEAT_INTERVAL = 2000; 
 
 void connectWiFi() {
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 }
 
 void connectMQTT() {
@@ -44,6 +49,7 @@ void setup() {
   for (int i = 0; i < pirCount; i++) {
     pinMode(pirPins[i], INPUT_PULLDOWN);
     lastState[i] = LOW;
+    lastSend[i] = 0;
   }
 
   Serial.println("PIR ESP READY: " ESP_ID);
@@ -53,22 +59,34 @@ void loop() {
   if (!client.connected()) connectMQTT();
   client.loop();
 
+  unsigned long now = millis();
+
   for (int i = 0; i < pirCount; i++) {
     int currentState = digitalRead(pirPins[i]);
 
-    if (currentState != lastState[i]) {
-      String topic = "powernest/" + String(ESP_ID) +
-                     "/pir/" + String(pirPins[i]);
+    String topic = "powernest/" + String(ESP_ID) +
+                   "/pir/" + String(pirPins[i]);
 
-      client.publish(
-        topic.c_str(),
-        currentState == HIGH ? "active" : "stopped"
-      );
-
-      Serial.println(topic + " -> " +
-        (currentState == HIGH ? "active" : "stopped"));
-
-      lastState[i] = currentState;
+    if (currentState == HIGH && lastState[i] == LOW) {
+      client.publish(topic.c_str(), "active");
+      lastSend[i] = now;
+      Serial.println(topic + " -> active");
     }
+
+    else if (currentState == HIGH &&
+             now - lastSend[i] >= HEARTBEAT_INTERVAL) {
+      client.publish(topic.c_str(), "active");
+      lastSend[i] = now;
+      Serial.println(topic + " -> active");
+    }
+
+    else if (currentState == LOW && lastState[i] == HIGH) {
+      client.publish(topic.c_str(), "stopped");
+      Serial.println(topic + " -> stopped");
+    }
+
+    lastState[i] = currentState;
   }
+
+  delay(50);
 }
