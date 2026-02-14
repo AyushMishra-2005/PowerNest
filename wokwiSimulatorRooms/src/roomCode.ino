0e8412d7eccd22721021b9d9286d53a7a039f02c
AyushMishraFeb2005 <ayushmishra9449@gmail.com>
2026-02-14 22:12:43 +0530

    feat: implement active pin state tracking and send active pin array to server on change and at fixed intervals

--- wokwiSimulatorRooms/src/roomCode.ino
+++ wokwiSimulatorRooms/src/roomCode.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #define ESP_ID "ESP_RELAY_ROOM_101"
 
@@ -19,12 +20,57 @@ int relayPins[] = {
 
 int pinCount = sizeof(relayPins) / sizeof(relayPins[0]);
 
+
 unsigned long lastActiveTime[40];
-bool manualHold[40];                 
+bool manualHold[40];
 const unsigned long RELAY_TIMEOUT = 4000;
+
+bool activePinsState[40];      
+bool lastSentState[40];        
+
+unsigned long lastStatusSend = 0;
+const unsigned long STATUS_INTERVAL = 4000;
+
+
+void sendActivePins() {
+
+  StaticJsonDocument<256> doc;
+  doc["espId"] = ESP_ID;
+
+  JsonArray arr = doc.createNestedArray("activePins");
+
+  for (int i = 0; i < pinCount; i++) {
+    int pin = relayPins[i];
+    if (activePinsState[pin]) {
+      arr.add(pin);
+    }
+  }
+
+  char buffer[256];
+  serializeJson(doc, buffer);
+
+  client.publish("powernest/status/ESP_RELAY_ROOM_101", buffer);
+  Serial.println("Status sent to server");
+}
+
+void copyCurrentToLast() {
+  for (int i = 0; i < 40; i++) {
+    lastSentState[i] = activePinsState[i];
+  }
+}
+
+bool hasChanged() {
+  for (int i = 0; i < 40; i++) {
+    if (activePinsState[i] != lastSentState[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 
 
 void callback(char* topic, byte* payload, unsigned int length) {
+
   String msg;
   for (int i = 0; i < length; i++) {
     msg += (char)payload[i];
@@ -36,23 +82,27 @@ void callback(char* topic, byte* payload, unsigned int length) {
 
   if (msg == "ON") {
     digitalWrite(pin, HIGH);
-    manualHold[pin] = false;          
+    manualHold[pin] = false;
     lastActiveTime[pin] = millis();
+    activePinsState[pin] = true;   
     Serial.println("Relay PIN " + String(pin) + " -> ON (AUTO)");
   }
 
   else if (msg == "ON_MANUAL") {
     digitalWrite(pin, HIGH);
-    manualHold[pin] = true;           
+    manualHold[pin] = true;
+    activePinsState[pin] = true;   
     Serial.println("Relay PIN " + String(pin) + " -> ON (MANUAL)");
   }
 
   else if (msg == "OFF") {
     digitalWrite(pin, LOW);
     manualHold[pin] = false;
+    activePinsState[pin] = false;  
     Serial.println("Relay PIN " + String(pin) + " -> OFF");
   }
 }
+
 
 void connectWiFi() {
   WiFi.begin(ssid, password);
@@ -73,14 +123,19 @@ void connectMQTT() {
 
 
 void setup() {
+
   Serial.begin(115200);
 
   for (int i = 0; i < pinCount; i++) {
     int pin = relayPins[i];
+
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
+
     lastActiveTime[pin] = 0;
-    manualHold[pin] = false;          
+    manualHold[pin] = false;
+    activePinsState[pin] = false;
+    lastSentState[pin] = false;
   }
 
   connectWiFi();
@@ -93,6 +148,7 @@ void setup() {
 
 
 void loop() {
+
   if (!client.connected()) connectMQTT();
   client.loop();
 
@@ -106,8 +162,20 @@ void loop() {
         now - lastActiveTime[pin] > RELAY_TIMEOUT) {
 
       digitalWrite(pin, LOW);
+      activePinsState[pin] = false;   
       Serial.println("Relay PIN " + String(pin) + " -> AUTO OFF");
     }
+  }
+
+  if (hasChanged()) {
+    sendActivePins();
+    copyCurrentToLast();
+  }
+
+  if (now - lastStatusSend > STATUS_INTERVAL) {
+    sendActivePins();
+    copyCurrentToLast();
+    lastStatusSend = now;
   }
 
   delay(10);
